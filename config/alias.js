90af311a2f0fcb12229245ffdc10ccbd686b309a
Tzu Yin <tzyn.wang@gmail.com>
2022-07-28 22:14:23 +0800

    [chore] set alias

--- config/alias.js
+++ config/alias.js
@@ -1,5 +1,7 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  "@Components": path.resolve(__dirname, '..', './src/components')
-}
+  '@Components': path.resolve(__dirname, '..', './src/components'),
+  '@Hooks': path.resolve(__dirname, '..', './src/hooks'),
+  '@Models': path.resolve(__dirname, '..', './src/models'),
+};
